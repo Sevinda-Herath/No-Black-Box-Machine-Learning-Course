c967a380d48d29c3b8570ea86199fa4e0d35277f
Sevinda-Herath <sevindaherath@gmail.com>
2023-11-25 13:11:07 +0530

    Change-4

--- js/sketchPad.js
+++ js/sketchPad.js
@@ -8,6 +8,21 @@ class SketchPad{
             box-shadow: 0px 0px 10px 2px black;
         `;
         container.appendChild(this.canvas);
+
+        this.ctx=this.canvas.getContext("2d");
+
+        this.#addEventListeners();
+    }
+
+    #addEventListeners(){
+        this.canvas.onmousedown=(evt)=>{
+            const rect=this.canvas.getBoundingClientRect();
+            const mouse=[
+                evt.clientX-rect.left,
+                evt.clientY-rect.top
+            ];
+            console.log(mouse);
+        }
     }
     
 }
